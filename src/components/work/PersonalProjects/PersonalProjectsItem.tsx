c42123b4ab025294fada60020d6068afbcefe7c9
Samy Bahi <samy@samybahi.com>
2023-08-26 09:56:01 +0200

    updated project card

--- src/components/work/PersonalProjects/PersonalProjectsItem.tsx
+++ src/components/work/PersonalProjects/PersonalProjectsItem.tsx
@@ -10,12 +10,7 @@ type PersonalProjectsItemProps = {
 const PersonalProjectsItem = ({ projectData }: PersonalProjectsItemProps) => {
   return (
     <li className="lg:w-[900px] border rounded-md flex flex-col border-opacity-20 border-secondary space-y-10">
-      <a
-        href={projectData.attributes.appLink.data?.attributes.url || "#"}
-        target="_blank"
-        rel="noreferrer"
-        className="flex flex-col space-y-6 lg:flex-row p-3 lg:space-x-6 lg:space-y-0"
-      >
+      <div className="flex flex-col space-y-6 lg:flex-row p-3 lg:space-x-6 lg:space-y-0">
         <div className="w-full lg:w-1/3 aspect-video rounded shrink-0 relative overflow-hidden">
           <Image
             src={
@@ -32,13 +27,22 @@ const PersonalProjectsItem = ({ projectData }: PersonalProjectsItemProps) => {
         <div className="w-full">
           <div className="display flex justify-between items-center lg:items-start">
             <h2 className="text-lg md:text-2xl font-bold">
-              {projectData.attributes.title}
+              <a
+                href={
+                  projectData.attributes.appLink.data?.attributes.url || "#"
+                }
+                target="_blank"
+                rel="noreferrer"
+                className="hover:underline"
+              >
+                {projectData.attributes.title}
+              </a>
             </h2>
             <LinkIcon className="opacity-50" />
           </div>
           <p className="opacity-50">{projectData.attributes.description}</p>
         </div>
-      </a>
+      </div>
       <div className="border-t border-secondary border-opacity-20 flex justify-between items-center py-1 px-5 ">
         {projectData.attributes.tags.data && (
           <TagsList tags={projectData.attributes.tags.data} />
